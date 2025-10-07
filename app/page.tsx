5a3d9976e388691fdad97879c62a9616b3edab29
Agrika <agrika.gupta@sjsu.edu>
2025-10-07 16:05:19 -0700

    Some UI changes to auth page Changes to be committed: 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,14 +29,13 @@ export default async function Home() {
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+          <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
               <Link href={"/"}>JobAI</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
             </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+            <div>
+              Find jobs faster and smarter, tailored just for you.
+            </div>
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
